47ca66a5d3b441080faaa50e63a9737e05adbd56
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2024-08-25 22:09:04 +0330

    fix: add loading state for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,3 @@
-// import logoSvg from "../../public/images/barnomz-horizontal-logo.svg";
 import BBtn from "@/components/dls/BBtn";
 import { signOut, useSession } from "next-auth/react";
 import NavBarMenu from "@/components/NavBarMenu";
@@ -8,13 +7,19 @@ import { useState } from "react";
 export default function NavBar() {
   const [isLoading, setIsLoading] = useState(false);
   const session = useSession();
-  const isLoggedIn = !!session?.data;
 
   const logout = () => {
     setIsLoading(true);
-    signOut({ callbackUrl: "/auth/login" });
+    signOut({ callbackUrl: "/auth/login" }).then();
     setIsLoading(false);
   };
+
+  const navBarEndLoading = (
+    <div className="flex gap-4">
+      <div className="h-[2.25rem] w-[4.5rem] animate-pulse rounded bg-white/10"></div>
+      <div className="h-[2.25rem] w-[4.5rem] animate-pulse rounded bg-white/10"></div>
+    </div>
+  );
 
   const navBarEndLoggedIn = (
     <BBtn
@@ -41,21 +46,18 @@ export default function NavBar() {
   return (
     <div className="sticky flex h-[3.75rem] w-full items-center justify-between bg-primary/50 px-[1.5rem] py-[0.75rem] backdrop-blur">
       <Link href="/">
-        {/*<Image*/}
-        {/*  src={logoSvg}*/}
-        {/*  alt="Barnomz Logo"*/}
-        {/*  width={120}*/}
-        {/*  height={36}*/}
-        {/*  priority*/}
-        {/*/>*/}
         <img
           src="/images/barnomz-horizontal-logo.svg"
           alt="barnomz logo"
           width="100px"
         />
       </Link>
-      {isLoggedIn && <NavBarMenu />}
-      {isLoggedIn ? navBarEndLoggedIn : navBarEndLoggedOut}
+      {session.status === "authenticated" && <NavBarMenu />}
+      <div className="flex items-center gap-2">
+        {session.status === "loading" && navBarEndLoading}
+        {session.status === "authenticated" && navBarEndLoggedIn}
+        {session.status === "unauthenticated" && navBarEndLoggedOut}
+      </div>
     </div>
   );
 }
