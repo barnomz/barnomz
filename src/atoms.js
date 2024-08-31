c04b4dc403464fddb2880f651c853b444df832e5
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2024-08-31 21:40:34 +0330

    fix: change order of atoms

--- src/atoms.js
+++ src/atoms.js
@@ -1,7 +1,7 @@
 import { atomWithStorage } from "jotai/utils";
 
-export const currentScheduleIdAtom = atomWithStorage("currentScheduleId", 0);
-
 export const schedulesAtom = atomWithStorage("schedules", [
   { id: 0, courses: [] },
 ]);
+
+export const currentScheduleIdAtom = atomWithStorage("currentScheduleId", 0);
