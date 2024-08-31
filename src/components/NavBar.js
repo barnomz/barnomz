d9216ae97f8a844572d0f29b8cee1233ee3dc22f
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2024-08-31 22:53:22 +0330

    fix: add tooltip for buttons in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,27 @@
 import NavBarMenu from "@/components/NavBarMenu";
 import Link from "next/link";
-import BBtn from "@/components/dls/BBtn.js";
+import BBtn from "@/components/dls/BBtn";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faInfo } from "@fortawesome/free-solid-svg-icons";
 import InfoDialog from "@/components/InfoDialog";
 import { useState } from "react";
+import { cn } from "@/utils/helpers";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+
+  const tooltip = (title, className) => {
+    return (
+      <span
+        className={cn(
+          "top-13 fixed z-50 mt-1 w-max rounded bg-black p-2 text-xs text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100",
+          className,
+        )}
+      >
+        {title}
+      </span>
+    );
+  };
 
   return (
     <div className="sticky flex h-[3.75rem] w-full items-center justify-between bg-primary/50 px-[1.5rem] py-[0.75rem] backdrop-blur">
@@ -20,21 +34,27 @@ export default function NavBar() {
       </Link>
       <NavBarMenu />
       <div className="flex items-center gap-2">
-        <BBtn
-          to="https://github.com/barnomz/barnomz"
-          icon={faGithub}
-          target="_blank"
-          iconSize="2xl"
-          color="primary"
-          className="!p-2"
-        />
-        <BBtn
-          icon={faInfo}
-          iconSize="xl"
-          color="primary"
-          className="!py-[11.5px]"
-          onClick={() => setIsOpen(true)}
-        />
+        <div className="group relative">
+          <BBtn
+            to="https://github.com/barnomz/barnomz"
+            icon={faGithub}
+            target="_blank"
+            iconSize="2xl"
+            color="primary"
+            className="!p-2"
+          />
+          {tooltip("گیت‌هاب", "left-16")}
+        </div>
+        <div className="group relative">
+          <BBtn
+            icon={faInfo}
+            iconSize="xl"
+            color="primary"
+            className="!py-[11.5px]"
+            onClick={() => setIsOpen(true)}
+          />
+          {tooltip("درباره ما", "left-4")}
+        </div>
       </div>
 
       <InfoDialog isOpen={isOpen} onClose={() => setIsOpen(false)} />
