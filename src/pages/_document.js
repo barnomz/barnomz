eda8201e41f39e30dcc5e72d2c9792d4dd8cefaf
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2024-08-31 23:37:48 +0330

    fix: make the code related to user-agent null-safety

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,11 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import { isMobileOrTablet } from "@/utils/helpers.js";
 
 function Document(props) {
   return (
     <Html lang="fa" className="bg-primary-dark">
       <Head>
-        {(props.userAgent.isMobile || props.userAgent.isTablet) && (
+        {(props.userAgent?.isMobile || props.userAgent?.isTablet) && (
           <meta name="viewport" content="width=1280" />
         )}
         <link rel="shortcut icon" href="/images/barnomz-logo.svg" />
@@ -47,14 +48,13 @@ function Document(props) {
 
 Document.getInitialProps = async (ctx) => {
   const initialProps = await ctx.defaultGetInitialProps(ctx);
-  const userAgent = ctx.req.headers["user-agent"];
+  const userAgent = ctx.req?.headers["user-agent"];
+
+  if (!userAgent) return initialProps;
 
   return {
     ...initialProps,
-    userAgent: {
-      isMobile: /mobile/i.test(userAgent),
-      isTablet: /tablet/i.test(userAgent),
-    },
+    userAgent: isMobileOrTablet(userAgent),
   };
 };
 
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,27 +1,44 @@
-import { twMerge } from 'tailwind-merge'
-import { clsx } from 'clsx'
-import { persianNumbers, arabicNumbers, iPhoneNumbers } from '@/constants/const'
+import { twMerge } from "tailwind-merge";
+import { clsx } from "clsx";
+import {
+  persianNumbers,
+  arabicNumbers,
+  iPhoneNumbers,
+} from "@/constants/const";
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function convertPersianNumberToEnglish(number) {
   Array.from(Array(10).keys()).forEach((_, i) => {
-    const numbers = [persianNumbers[i], arabicNumbers[i], iPhoneNumbers[i]]
-    const regexp = new RegExp(numbers.join('|'), 'g')
-    number = number.replace(regexp, i)
-  })
-  return number
+    const numbers = [persianNumbers[i], arabicNumbers[i], iPhoneNumbers[i]];
+    const regexp = new RegExp(numbers.join("|"), "g");
+    number = number.replace(regexp, i);
+  });
+  return number;
 }
 
 export function convertEnglishNumberToPersian(number) {
   return String(number).replace(
     /\d/g,
     (digit) => persianNumbers[parseInt(digit)],
-  )
+  );
 }
 
 export function getDaysOfWeek(days) {
-  return days.map((day) => (day === 0 ? 6 : day - 1))
+  return days.map((day) => (day === 0 ? 6 : day - 1));
+}
+
+export function isMobileOrTablet(userAgent) {
+  const ua = userAgent.toLowerCase();
+  const isMobile =
+    /mobile|iphone|ipod|android.*mobile|blackberry|windows phone|opera mini/.test(
+      ua,
+    );
+  const isTablet = /ipad|android(?!.*mobile)|tablet/.test(ua);
+  return {
+    isMobile,
+    isTablet,
+  };
 }
