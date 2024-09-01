62fe2fa330cab9a513fc1fb6b09a972882c2e1cd
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2024-09-01 17:40:07 +0330

    fix: update style of TooltipContent

--- src/components/schedule/TooltipContent.js
+++ src/components/schedule/TooltipContent.js
@@ -42,24 +42,70 @@ const TooltipContent = ({ course, inSchedule = false }) => {
     return formatter.format(new Date(jDateTime)).toString();
   };
 
+  const courseDetails = [
+    {
+      label: "استاد",
+      value: course.presentedBy,
+      enabled: true,
+      className: "",
+    },
+    {
+      label: "ظرفیت",
+      value: course.numberOfCapacity,
+      enabled: true,
+      className: "",
+    },
+    {
+      label: "تعداد ثبت‌نامی",
+      value: course.numberOfEnrolled,
+      enabled: true,
+      className: "",
+    },
+    {
+      label: "تاریخ امتحان",
+      value: getCourseFinalExamDateAndTime(course) || "-",
+      enabled: true,
+      className: "",
+    },
+    {
+      label: "توضیحات",
+      value: course.info || "-",
+      enabled: !!course.info,
+      className: "",
+    },
+    {
+      label: null,
+      value: "تلاقی امتحان با دروس انتخابی دارد.",
+      enabled: !inSchedule && examConflict && !isInSchedule,
+      className: "pt-2 text-warning",
+    },
+    {
+      label: null,
+      value: "این درس به برنامه اضافه شده است.",
+      enabled: isInSchedule,
+      className: "pt-2 text-success",
+    },
+  ];
+
   return (
     <div className="flex w-full flex-col gap-1">
-      <span className="line-clamp-1 text-sm font-bold">{`${course.courseName} (${course.group}-${course.courseCode})`}</span>
-      <span>{`استاد: ${course.presentedBy}`}</span>
-      <span>{`ظرفیت: ${course.numberOfCapacity}`}</span>
-      <span>{`تعداد ثبت‌نامی: ${course.numberOfEnrolled}`}</span>
-      <span>{`تاریخ امتحان: ${getCourseFinalExamDateAndTime(course) || "-"}`}</span>
-      {course.info && <span>{`توضیحات: ${course.info || "-"}`}</span>}
-      {!inSchedule && examConflict && !isInSchedule && (
-        <div className="pt-2 text-warning">
-          تلاقی امتحان با دروس انتخابی دارد.
-        </div>
-      )}
-      {isInSchedule && (
-        <div className="pt-2 text-success">
-          این درس به برنامه اضافه شده است.
-        </div>
-      )}
+      <div className="flex items-center gap-2 text-sm font-bold">
+        <span className="whitespace-nowrap text-white/60">درس:</span>
+        <span className="line-clamp-1 flex-1">{`${course.courseName} (${course.group}-${course.courseCode})`}</span>
+      </div>
+      {courseDetails
+        .filter((detail) => detail.enabled)
+        .map((detail, index) => (
+          <div
+            key={index}
+            className={`flex items-center gap-2 ${detail.className}`}
+          >
+            {detail.label && (
+              <span className="whitespace-nowrap text-white/60">{`${detail.label}:`}</span>
+            )}
+            <span className="flex-1">{detail.value}</span>
+          </div>
+        ))}
     </div>
   );
 };
